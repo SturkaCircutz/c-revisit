c0d9c9c1418e8275823a6f6ea5a8e63f134055e5
SturkaCircutz <313721325sjw@gmail.com>
2026-09-13 22:19:59 -0600

    new

--- unions_structs.c
+++ unions_structs.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
-    int data;
-    struct Node * next;
-};
-struct Node* add_back(struct Node * head, int num){
-    struct Node *new_node = malloc(sizeof(struct Node));
-    new_node->data = num;
-    new_node->next = head;
-    head = new_node;
-    return head;
-}
-int main(){
-    struct Node * head = malloc(sizeof(struct Node));
-    head->data = 10;
-    head->next = NULL;
-    
-    int num;
-    printf("give a number: \n");
-    scanf("%d", &num);
-    printf("%d", add_back(head, num)->data);
+struct Node { int data; struct Node *next; };
 
+void push(struct Node **head_ref, int new_val) {
+    struct Node *new_node = malloc(sizeof(struct Node));
+    new_node->data = new_val;
+    new_node->next = *head_ref; // New node points to old head
+    *head_ref = new_node;       // Head now points to new node
+}
+
+void printList(struct Node *n) {
+    while (n != NULL) { printf("%d -> ", n->data); n = n->next; }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node *head = malloc(sizeof(struct Node));
+    head->data = 10; head->next = NULL;
+
+    printf("Existing List: "); printList(head);
+    push(&head, 20);
+    printf("After Push(20): "); printList(head);
+    return 0;
 }
